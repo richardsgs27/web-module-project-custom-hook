0f7a0c874159fdbae02b78de4552cd970a0ed7e0
Gigi <gigi@iMacs-iMac.local>
2022-03-21 02:21:30 -0500

    done

--- frontend/components/hooks/useDarkMode.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function useDarkMode() {
-   const [dark, setDark] = useState(false)
-   const onChange = ({target: {value}}) => {
-       setDark(!value)
-   }
-   return [dark,onChange]
-}
